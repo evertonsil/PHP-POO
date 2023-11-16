3bed715cc801c2e996c647974fee0f22031f1f25
evertonsil <everton.marques@uni9.edu.br>
2023-11-16 14:22:03 -0300

    criado classe lutador

--- aula-007/classes/Lutador.php
+++ aula-007/classes/Lutador.php
@@ -13,8 +13,15 @@ class Lutador
     private $empates;
 
     //Metodos especiais
-    public function __construct() {
-
+    public function __construct($nome, $nacionalidade, $idade, $altura, $peso, $vitorias, $derrotas, $empates) {
+        $this->nome          = $this->setNome($nome);
+        $this->nacionalidade = $this->setNacionalidade($nacionalidade);
+        $this->idade         = $this->setIdade($idade);
+        $this->altura        = $this->setAltura($altura);
+        $this->peso          = $this->setPeso($peso);
+        $this->vitorias      = $this->setVitorias($vitorias);
+        $this->derrotas      = $this->setDerrotas($derrotas);
+        $this->empates       = $this->setEmpates($empates);
     }
 
     public function getNome() {
@@ -50,13 +57,29 @@ class Lutador
     }
     public function setPeso($peso) {
         $this->peso = $peso;
+        $this->setCategoria($peso);
         return $this;
     }
     public function getCategoria() {
         return $this->categoria;
     }
-    public function setCategoria($categoria) {
-        $this->categoria = $categoria;
+    public function setCategoria($peso) {
+        if ($this->peso < 52.2)
+        {
+            $this->setCategoria("Inválido");
+        } else if ($this->peso <= 70.3)
+        {
+            $this->setCategoria("Leve");
+        } else if ($this->peso <= 83.9)
+        {
+            $this->setCategoria("Médio");
+        } else if ($this->peso <= 120.2)
+        {
+            $this->setCategoria("Pesado");
+        } else
+        {
+            $this->setCategoria("Inválido");
+        }
         return $this;
     }
     public function getVitorias() {
@@ -83,18 +106,35 @@ class Lutador
 
     //Metodos
     public function apresentar() {
-
+        echo "<br>Nome: $this->getNome()";
+        echo "<br>Nacionalidade: $this->getNacionalidade()";
+        echo "<br>Idade: $this->getIdade()";
+        echo "<br>Altura: $this->getAltura()";
+        echo "<br>Peso: $this->getPeso()";
+        echo "<br>Categoria: $this->getCategoria()";
+        echo "<br>Vitórias: $this->getVitorias()";
+        echo "<br>Derrotas: $this->getDerrotas()";
+        echo "<br>Empates: $this->getEmpates()";
     }
     public function status() {
-
+        echo "<br>Nome: $this->getNome()";
+        echo "<br>Categoria: $this->getCategoria()";
+        echo "<br>Vitórias: $this->getVitoria()";
+        echo "<br>Derrotas: $this->getDerrotas()";
     }
     public function ganharLuta() {
-
+        $vitorias = $this->getVitorias();
+        $vitorias = $vitorias + 1;
     }
     public function perderLuta() {
-
+        $vitorias = $this->getVitorias();
+        if ($vitorias >= 0)
+        {
+            $vitorias = $vitorias - 1;
+        }
     }
     public function empatarLuta() {
-
+        $empates = $this->getEmpates();
+        $empates = $empates + 1;
     }
 }
